ca3e2698ee5ebc31e938a653abc7e86846a5bf96
Josxiu <100869137+Josxiu@users.noreply.github.com>
2025-06-11 19:02:26 -0500

    Update extend.sv
    Se modifica el extend para hacer rotaciones

--- Práctica 5/extend.sv
+++ Práctica 5/extend.sv
@@ -4,10 +4,22 @@
 module extend(input logic [23:0] Instr,
 				  input logic [1:0] ImmSrc,
 				  output logic [31:0] ExtImm);
+
+	logic [7:0] Imm8;
+	logic [3:0] rot;
+	
 	always_comb
 		case(ImmSrc)
 			// 8-bit unsigned immediate
-			2'b00: ExtImm = {24'b0, Instr[7:0]};
+			2'b00: begin
+				Imm8 = Instr[7:0];
+				rot = Instr[11:8];
+				// Se rota a la derecha el inmediato
+				if (rot == 0)
+					ExtImm = {24'b0, Imm8}; // No rotación
+				else
+					ExtImm = ({24'b0, Imm8} >> (rot*2)) | ({24'b0, Imm8} << (32-(rot*2))); // Rotación a la derecha
+			end
 			// 12-bit unsigned immediate
 			2'b01: ExtImm = {20'b0, Instr[11:0]};
 			// 24-bit two's complement shifted branch
